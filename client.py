02559fc9dfed4edff350107ec8751aef69199c13
wxiwnd <g1243946429@gmail.com>
2024-05-18 01:47:59 +0800

    Add Crypto Feature

--- client.py
+++ client.py
@@ -1,20 +1,42 @@
-import asyncio, struct, os
+import asyncio, struct, hashlib, os
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad
 
 trustid = os.getenv("TRUST_ID")
+
+secret = hashlib.sha256(trustid.encode()).digest()
+key = secret[:16]
+iv = secret[16:]
+
 
 async def main():
     reader, writer = await asyncio.open_connection(
         '127.0.0.1', 20049)
+    
+    # Cipher Init
 
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+
+    # Build Hello Packet
     hello_flag = struct.pack("B", 0x00)
-    hello_trustid_len = struct.pack("B", len(trustid))
-    hello_packet = hello_flag + hello_trustid_len + trustid.encode()
+    
+    # Encrypt TRUST_ID
+    hello_trustid = cipher.encrypt(pad(trustid.encode(), AES.block_size))
+    hello_trustid_len = struct.pack("B", len(hello_trustid))
+    hello_packet = hello_flag + hello_trustid_len + hello_trustid
 
 
+    # BUild Data Packet
     data_flag = struct.pack("B", 0x01)
-    data = "HelloWorld"
+    original_data = "HelloWorld"
+
+    # Encrypt the Data
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    print(AES.block_size)
+    data = cipher.encrypt(pad(original_data.encode(), AES.block_size))
     data_len = struct.pack("I", len(data))
-    data_packet = data_flag + data_len + data.encode()
+    data_packet = data_flag + data_len + data
+
 
     writer.write(hello_packet)
     await writer.drain()
--- server.py
+++ server.py
@@ -1,9 +1,16 @@
-import asyncio, struct, os
+import asyncio, struct, hashlib, os
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import unpad
 
 trustid = os.getenv("TRUST_ID")
 
+secret = hashlib.sha256(trustid.encode()).digest()
+key = secret[:16]
+iv = secret[16:]
+
 async def handle_hello(reader, writer):
     addr = writer.get_extra_info('peername')
+    cipher = AES.new(key, AES.MODE_CBC, iv)
 
     flag = await reader.read(1)
     flag = struct.unpack("B", flag)[0]
@@ -12,13 +19,14 @@ async def handle_hello(reader, writer):
         client_trustid_len = await reader.read(1)
         client_trustid_len = struct.unpack("B", client_trustid_len)[0]
         client_trustid = await reader.read(client_trustid_len)
-        client_trustid = client_trustid.decode()
+        client_trustid = unpad(cipher.decrypt(
+            client_trustid), AES.block_size).decode()
+        
 
         if (client_trustid == trustid):
             print(f"Connection Established from {addr!r}")
             while True:
                 if await handle_data(reader, writer) == -1: break
-                
 
         else:
             print("Invalid TRUST_ID, Closing Connection...")
@@ -33,6 +41,8 @@ async def handle_hello(reader, writer):
     
 
 async def handle_data(reader, writer):
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+
     flag = await reader.read(1)
     if not flag:
         return -1
@@ -40,21 +50,23 @@ async def handle_data(reader, writer):
     if (flag == 0x01):
         data_length = await reader.read(4)
         data_length = struct.unpack("I", data_length)[0]
+        print(data_length)
 
-        data = await reader.read(data_length)
-        
+        data_encrypt = await reader.read(data_length)
+        data = unpad(cipher.decrypt(
+            data_encrypt), AES.block_size).decode()
         print(f"Received Data: {data!r}")
     return 0
 
 
 async def main():
-    print("Socket Cat Server, version alpha_0001")
+    print("SocketCat Server, version alpha_0001")
     print("All rights not reserved.")
 
     assert trustid, "TRUST_ID must be set and cannot be empty"    
 
     server = await asyncio.start_server(
-        handle_hello, '127.0.0.1', 20049)
+        handle_hello, '0.0.0.0', 20049)
 
     addrs = ', '.join(str(sock.getsockname()) for sock in server.sockets)
     print(f'Serving on {addrs}')
