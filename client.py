8413789f2d11b94d3507a19136dd2c47841fd4fc
wxiwnd <g1243946429@gmail.com>
2024-05-30 21:35:56 +0800

    Use some new feature of python 3.11

--- client.py
+++ client.py
@@ -1,50 +1,8 @@
-import asyncio, struct, os
-from packet import HelloPacket, DataPacket
-from utils import PacketUtils
-
-# Should use new socketcat class method but lazy to refactor(lack of time)
-async def main(address="localhost", port=20049, trustid=os.getenv("TRUST_ID")):
-    reader, writer = await asyncio.open_connection(
-        address, port)
-    
-    hello_packet = HelloPacket(trustid)
-    writer.write(hello_packet.pack())
-    await writer.drain()
-
-    # Wait for response
-    flag = await reader.read(1)
-    flag = struct.unpack("B", flag)[0]
-    if (flag == 0x01):
-        data_length = await reader.read(4)
-        data_length = struct.unpack("I", data_length)[0]
-        data_encrypt = await reader.read(data_length)
-        data = PacketUtils.decrypt(trustid, data_encrypt)
-
-        print(f'Receive: {data!r}')
-
-        if (data == b'403'):
-            print("Forbidden")
+from socketcat import SocketCatClient
+import asyncio
+scc = SocketCatClient('127.0.0.1', 20049, 'very_trust_id')
 
 
-    data_packet = DataPacket(trustid, b'Hello,World!')
-    datapack = data_packet.pack()
-    
-    writer.write(datapack)
-    await writer.drain()
-    print(f'Send data: {datapack!r}')
-    writer.write(datapack)
-    await writer.drain()
-    print(f'Send data: {datapack!r}')
-    writer.write(datapack)
-    await writer.drain()
-    print(f'Send data: {datapack!r}')
+scc.open_connection()
+scc.interactive()
 
-    print('Close the connection')
-    writer.close()
-    await writer.wait_closed()
-
-def connect(address, port, trust_id):
-    asyncio.run(main(address, port, trust_id))
-
-if __name__ == '__main__':
-    asyncio.run(main())
